52551dc70c1b1e9f8db09ab308c0a460701a514d
Ali Eslam <alizaeda92@gmail.com>
2021-09-30 04:47:52 +0200

    Finished Gatsby Tutorial Part 4

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,21 +1,27 @@
-import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
+import { graphql } from "gatsby";
 
-export default function blog() {
-  const data = useStaticQuery(graphql`
-    query MyQuery {
-      allFile {
-        nodes {
-          name
-        }
-      }
-    }
-  `);
-
+const Blog = ({ data }) => {
   return (
     <Layout>
-      <p>My cool posts will go in here</p>
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
-}
+};
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+
+export default Blog;
