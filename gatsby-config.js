215618d7a96c53ff9c4b217bf524dbf000d66382
Ali Eslam <alizaeda92@gmail.com>
2021-09-30 02:36:33 +0200

    Finished Gatsby Tutorial Part 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    siteUrl: "https://gatbsy.alieslam.tech/",
+    title: "My First Gatsby Site",
   },
   plugins: [],
 };
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
+
+const about = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
+};
+
+export default about;
